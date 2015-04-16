567c545434a460487eafd90794f8a97fe6929f0e
bttf <adnanchowdhury88@gmail.com>
2015-04-15 22:01:46 -0400

    rudimentary marker placement

--- app/components/add-event-toolbar.js
+++ app/components/add-event-toolbar.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   classNames: ['add-event-toolbar'],
   actions: {
     addEvent: function() {
+      this.set('event.date', new Date(this.get('event.date')));
       this.sendAction('add', this.get('event'));
     }
   }
--- app/components/main-map.js
+++ app/components/main-map.js
@@ -2,18 +2,36 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['main-map-wrapper'],
+  map: {},
+  bounds: new google.maps.LatLngBounds(),
   initMap: function() {
+    var self = this;
     var geocoder = new google.maps.Geocoder(); 
     geocoder.geocode( { 'address': 'Florida' }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         var mapOptions = {
-          zoom: 7,
+          zoom: 6,
           center: results[0].geometry.location
         };
-        var map = new google.maps.Map(document.getElementById('mainMap'), mapOptions);
+        self.set('map', new google.maps.Map(document.getElementById('mainMap'), mapOptions));
       } else {
         console.log('error');
       }
+
+      self.get('events').forEach(function(e) {
+        var address = e.get('city');
+        geocoder.geocode({ 'address': address }, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            var marker = new google.maps.Marker({
+              position: results[0].geometry.location,
+              map: self.get('map')
+            });
+            self.get('bounds').extend(results[0].geometry.location);
+            //self.get('map').fitBounds(self.get('bounds'));
+            self.get('map').setCenter(self.get('bounds').getCenter());
+          }
+        });
+      });
     });
-  }.on('didInsertElement')
+  }.on('didInsertElement'),
 });
