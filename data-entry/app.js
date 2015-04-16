47c06685c636f8490625e61469131c82faa16764
bttf <adnanchowdhury88@gmail.com>
2015-04-16 16:10:07 -0400

    fixed dating issues for data collection script

--- data-entry/app.js
+++ data-entry/app.js
@@ -5,18 +5,20 @@ fs.readFile('data.txt', function(err, data) {
   }
   var text = data.toString().split("\n");
   var events = [];
-  var date = new Date();
-  date.setYear(2015);
-
+  var month = 0;
   text.forEach(function(line) {
-    if (line.startsWith("April")) { date.setMonth(3); return; }
-    else if (line.startsWith("May")) { date.setMonth(4); return; }
-    else if (line.startsWith("June")) { date.setMonth(5); return; }
-    else if (line.startsWith("July")) { date.setMonth(6); return; }
-    else if (line.startsWith("August")) { date.setMonth(7); return; }
+    if (line.startsWith("April")) { month = 3; return; }
+    else if (line.startsWith("May")) { month = 4; return; }
+    else if (line.startsWith("June")) { month = 5; return; }
+    else if (line.startsWith("July")) { month = 6; return; }
+    else if (line.startsWith("August")) { month = 7; return; }
 
     line = line.trim();
     if (line === '') { return; }
+
+    var date = new Date();
+    date.setYear(2015);
+    date.setMonth(month);
 
     var day = parseInt(line.split(" ")[1]);
     date.setDate(day);
@@ -25,14 +27,12 @@ fs.readFile('data.txt', function(err, data) {
     var venue = line.split("-")[1].split("@")[1] || "";
     var city = line.split("-")[2] || "";
 
-    (function(d) {
-      events.push({
-        date: d,
-        artists: artists.trim(),
-        venue: venue.trim(),
-        city: city.trim()
-      });
-    })(date);
+    events.push({
+      date: date,
+      artists: artists.trim(),
+      venue: venue.trim(),
+      city: city.trim()
+    });
   });
 
   fs.writeFile('data.json', JSON.stringify(events, null, 2), function(err) {
